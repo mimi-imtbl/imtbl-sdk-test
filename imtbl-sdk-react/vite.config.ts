a1ec9b9c5cd118485360aea8cb0edbeaefe0be1b
Mimi Immutable <mimi.tran@immutable.com>
2025-01-09 15:23:27 +1100

    add polyfill

--- imtbl-sdk-react/vite.config.ts
+++ imtbl-sdk-react/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
